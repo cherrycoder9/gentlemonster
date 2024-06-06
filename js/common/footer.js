b8d093f59e4a998ae2e2f194545c4cfa46e5881d
cherrycoder9 <cherrycoder9@gmail.com>
2024-06-06 23:50:44 +0900

    dd

--- js/common/footer.js
+++ js/common/footer.js
@@ -1,1 +1,16 @@
-// 
+(function () {
+    const footerHTML = `
+        <footer>
+            <p>&copy; 2024 Your Company. All rights reserved.</p>
+            <nav>
+                <ul>
+                    <li><a href="/x-customer/update-user-info.html">Update Info</a></li>
+                    <li><a href="/x-customer/user-info.html">User Info</a></li>
+                    <li><a href="/x-customercare/faq.html">FAQ</a></li>
+                    <li><a href="/x-customercare/service-inquiry.html">Service Inquiry</a></li>
+                </ul>
+            </nav>
+        </footer>
+    `;
+    document.body.insertAdjacentHTML('beforeend', footerHTML);
+})();
--- js/common/header.js
+++ js/common/header.js
@@ -1,1 +1,15 @@
-// 
+(function () {
+    const headerHTML = `
+        <header>
+            <nav>
+                <ul>
+                    <li><a href="/index.html">Home</a></li>
+                    <li><a href="/x-customer/login.html">Login</a></li>
+                    <li><a href="/x-customer/signup.html">Sign Up</a></li>
+                    <li><a href="/x-customercare/customer-service.html">Customer Service</a></li>
+                </ul>
+            </nav>
+        </header>
+    `;
+    document.body.insertAdjacentHTML('afterbegin', headerHTML);
+})();
