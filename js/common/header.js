a05b99408a1f471629b2c0633f2aee86a49cd57b
cherrycoder9 <cherrycoder9@gmail.com>
2024-06-07 00:15:03 +0900

    ㄴㅇㄹㄴㅇ

--- js/common/header.js
+++ js/common/header.js
@@ -1,15 +1,13 @@
-(function () {
+loadHeader();
+function loadHeader() {
     const headerHTML = `
         <header>
             <nav>
                 <ul>
-                    <li><a href="/index.html">Home</a></li>
-                    <li><a href="/x-customer/login.html">Login</a></li>
-                    <li><a href="/x-customer/signup.html">Sign Up</a></li>
-                    <li><a href="/x-customercare/customer-service.html">Customer Service</a></li>
+                    <li><a href="/index.html">헤더 영역 테스트</a></li>
                 </ul>
             </nav>
         </header>
     `;
     document.body.insertAdjacentHTML('afterbegin', headerHTML);
-})();
+}
