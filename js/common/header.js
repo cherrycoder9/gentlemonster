d7ba281444dc4370cc58b4f0fb90abbf97931c56
cherrycoder9 <cherrycoder9@gmail.com>
2024-06-07 14:07:32 +0900

    sdfsdf

--- js/common/header.js
+++ js/common/header.js
@@ -1,13 +1,27 @@
 loadHeader();
 function loadHeader() {
     const headerHTML = `
-        <header>
-            <nav>
-                <ul>
-                    <li><a href="/index.html">헤더 영역 테스트</a></li>
-                </ul>
-            </nav>
-        </header>
+        <div id="logo">
+            <a href="/index.html">GENTLE MONSTER</a>
+        </div>
+        <div id="mainMenu">
+            <ul>
+                <li><a href="#">선글라스</a></li>
+                <li><a href="#">안경</a></li>
+                <li><a href="#">콜라보레이션</a></li>
+                <li><a href="#">스타일</a></li>
+                <li><a href="#">브랜드이슈</a></li>
+                <li><a href="#">매장보기</a></li>
+                <li><a href="#">수리서비스</a></li>
+            </ul>
+        </div>
+        <div id="utilMenu">
+            <ul>
+                <li><a href="#">ㅇ</a></li>
+                <li><a href="#">로그인</a></li>
+                <li><a href="#">ㅇ</a></li>
+            </ul>
+        </div>
     `;
-    document.body.insertAdjacentHTML('afterbegin', headerHTML);
+    document.querySelector('header').innerHTML = headerHTML;
 }
