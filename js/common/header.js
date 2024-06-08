977283d1a427b7d73f3980ffddbe4d3f72e9ef2a
cherrycoder9 <cherrycoder9@gmail.com>
2024-06-08 12:08:53 +0900

    상대적 경로에 따라 아이콘 표시 안되던 문제 수정

--- js/common/header.js
+++ js/common/header.js
@@ -1,13 +1,19 @@
 loadHeader();
 function loadHeader() {
-    const headerHTML = `
+    let headerHTML = '';
+    let currentPath = window.location.pathname;
+
+    console.log(currentPath);
+    if (currentPath.endsWith("/index.html") || currentPath == "/" || currentPath == "") {
+        document.body.classList.add("indexPage");
+        headerHTML = `
         <div id="logo">
             <a href="../index.html">GENTLE MONSTER</a>
         </div>
         <div id="mainMenu">
             <ul>
-                <li><a href="../x-customer/sunglasses.html">선글라스</a></li>
-                <li><a href="../x-customer/glasses.html">안경</a></li>
+                <li><a href="./x-customer/sunglasses.html">선글라스</a></li>
+                <li><a href="./x-customer/glasses.html">안경</a></li>
                 <li><a href="#"><s>콜라보레이션</s></a></li>
                 <li><a href="#"><s>스타일</s></a></li>
                 <li><a href="#"><s>브랜드이슈</s></a></li>
@@ -23,21 +29,41 @@ function loadHeader() {
             </ul>
         </div>
     `;
-    document.querySelector('header').innerHTML = headerHTML;
-}
+        document.querySelector('header').innerHTML = headerHTML;
+        let searchSvg = document.querySelector('#searchSvg');
+        let cartSvg = document.querySelector('#cartSvg');
+        searchSvg.src = './img/icon/search-white.svg';
+        cartSvg.src = './img/icon/cart-white.svg';
 
-(function () {
-    let currentPath = window.location.pathname;
-    let searchSvg = document.querySelector('#searchSvg');
-    let cartSvg = document.querySelector('#cartSvg');
-    console.log(currentPath);
-    if (currentPath.endsWith("/index.html") || currentPath == "/" || currentPath == "") {
-        document.body.classList.add("indexPage");
-        searchSvg.src = '../../img/icon/search-white.svg';
-        cartSvg.src = '../../img/icon/cart-white.svg';
     } else {
         document.body.classList.add("otherPage");
-        searchSvg.src = '../../img/icon/search-black.svg';
-        cartSvg.src = '../../img/icon/cart-black.svg';
+        headerHTML = `
+        <div id="logo">
+            <a href="../index.html">GENTLE MONSTER</a>
+        </div>
+        <div id="mainMenu">
+            <ul>
+                <li><a href="../x-customer/sunglasses.html">선글라스</a></li>
+                <li><a href="../x-customer/glasses.html">안경</a></li>
+                <li><a href="#"><s>콜라보레이션</s></a></li>
+                <li><a href="#"><s>스타일</s></a></li>
+                <li><a href="#"><s>브랜드이슈</s></a></li>
+                <li><a href="#"><s>매장보기</s></a></li>
+                <li><a href="../x-productservice/repair_guide.html">수리서비스</a></li>
+            </ul>
+        </div>
+        <div id="utilMenu">
+            <ul>
+                <li><a href="#"><img id="searchSvg" src="#"></img></a></li>
+                <li><a href="../x-customer/login.html">로그인</a></li>
+                <li><a href="#"><img id="cartSvg" src="#"></img></a></li>
+            </ul>
+        </div>
+    `;
+        document.querySelector('header').innerHTML = headerHTML;
+        let searchSvg = document.querySelector('#searchSvg');
+        let cartSvg = document.querySelector('#cartSvg');
+        searchSvg.src = '../img/icon/search-black.svg';
+        cartSvg.src = '../img/icon/cart-black.svg';
     }
-})();
+}
