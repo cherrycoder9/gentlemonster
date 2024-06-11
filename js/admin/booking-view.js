c57b6331a918816e979326d5f1e52afc3e7d78f6
yangjaeyeon05 <didwodus1234@gmail.com>
2024-06-11 11:42:03 +0900

    20240611 관리페이지 업데이트

--- js/admin/booking-view.js
+++ js/admin/booking-view.js
@@ -2,36 +2,36 @@ let urlParams = new URL(location.href).searchParams;
 let no = urlParams.get('no'); // 클릭된 게시물 번호
 console.log(no);
 
-let inquiryList = [];
+let repairList = [];
 
 let memberList = []; console.log(memberList);
 
-_inquiryPrint();
+_repairPrint();
 
-function _inquiryPrint() {
-    console.log('_inquiryPrint()');
+function _repairPrint() {
+    console.log('_repairPrint()');
     // *
-    inquiryList = JSON.parse(localStorage.getItem('inquiryList'));
-    if (inquiryList == null) { inquiryList = []; } console.log(inquiryList);
+    repairList = JSON.parse(localStorage.getItem('repairList'));
+    if (repairList == null) { repairList = []; } console.log(repairList);
 
     memberList = JSON.parse(localStorage.getItem('memberList'));
     if (memberList == null) { memberList = []; } console.log(memberList);
 
     // 1. 어디에
-    let inquiryBox = document.querySelector('#inquiryBox');
+    let repairBox = document.querySelector('#repairBox');
     // 2. 무엇을
     let html = ``; console.log(html);
 
     let findIndex = -1;
     let findId = 0;
 
-    for (let i = 0; i < inquiryList.length; i++) {
-        if (no == inquiryList[i].inquiryNum) { findIndex = i; break; }
+    for (let i = 0; i < repairList.length; i++) {
+        if (no == repairList[i].repairNum) { findIndex = i; break; }
     }
     console.log(findIndex);
 
     for (let i = 0; i < memberList.length; i++) {
-        if (inquiryList[findIndex].memberId == memberList[i].memberId) { findId = i; break; }
+        if (repairList[findIndex].memberId == memberList[i].memberId) { findId = i; break; }
     }
 
     console.log(findId);
@@ -50,27 +50,23 @@ function _inquiryPrint() {
                         <div>${memberList[findId].email}</div>
                     </div>
                     <div class="contentBox">
-                        <p>주제</p>
-                        <div>${inquiryList[findIndex].subject}</div>
-                    </div>
-                    <div class="contentBox">
-                        <p>제목</p>
-                        <div>${inquiryList[findIndex].title}</div>
+                        <p>모델명</p>
+                        <div>${repairList[findIndex].productName}</div>
                     </div>
                     <div class="contentBox">
                         <p>내용</p>
-                        <div>${inquiryList[findIndex].msg}</div>
+                        <div>${repairList[findIndex].content}</div>
                     </div>
                     <div class="contentBox">
                         <p>작성일</p>
-                        <div>${inquiryList[findIndex].date}</div>
+                        <div>${repairList[findIndex].date}</div>
                     </div>
             
             `;
 
     // 3. 출력
-    inquiryBox.innerHTML = html;
+    repairBox.innerHTML = html;
 }
 function _move() {
-    location.href = "../x-admin/inquiry-mngm.html";
+    location.href = "../x-admin/booking-mngm.html";
 }
--- js/admin/inquiry-mngm.js
+++ js/admin/inquiry-mngm.js
@@ -1,35 +1,61 @@
-let inquiryList = [];       
+let inquiryList = [];
 
-let memberList = [];        
+let memberList = [];
+
+
+memberList = JSON.parse(localStorage.getItem('memberList'));
+if (memberList == null) { memberList = []; };
+console.log(memberList);
+
+let loginMemberId = sessionStorage.getItem('loginMemberId'); console.log(loginMemberId);
+
+_checkAdmin();
+
+function _checkAdmin() {
+    console.log('_checkAdmin()');
+
+    let checkAdmin = false;
+
+    console.log(loginMemberId);
+
+    for (let a = 0; a < memberList.length; a++) {
+        if (loginMemberId == memberList[a].memberId) {
+            checkAdmin = memberList[a].isAdmin;
+        }
+    }
+    console.log(checkAdmin);
+
+    if (checkAdmin == false) {
+        alert('관리자만 접근 가능합니다.');
+        location.href = '../index.html';
+    }
+}
 
 _inquiry();
-function _inquiry(){    console.log('_inquiry()');
-
-    memberList = JSON.parse(localStorage.getItem('memberList'));
-    if(memberList==null){memberList=[]};
-    console.log(memberList);
+function _inquiry() {
+    console.log('_inquiry()');
 
     inquiryList = JSON.parse(localStorage.getItem('inquiryList'));
-    if(inquiryList==null){inquiryList=[]}; 
+    if (inquiryList == null) { inquiryList = []; };
     console.log(inquiryList);
 
     // 어디에
     let inquiryBox = document.querySelector('#inquiryBox');
 
     // 무엇을
-    let html = ``;                  console.log(html);
+    let html = ``; console.log(html);
 
-    let findId = -1;                console.log(findId);
+    let findId = -1; console.log(findId);
     let findIq = -1;
 
-    for( let i = 0 ; i < inquiryList.length ; i++ ){
-        for(let j = 0 ; j < memberList.length ; j++ ){
-            if( inquiryList[i].memberId == memberList[j].memberId){
-                findId = j; 
+    for (let i = 0; i < inquiryList.length; i++) {
+        for (let j = 0; j < memberList.length; j++) {
+            if (inquiryList[i].memberId == memberList[j].memberId) {
+                findId = j;
                 findIq = i;
             }
         }
-        html +=`
+        html += `
                     <tr>
                         <td> ${inquiryList[findIq].inquiryNum} </td>
                         <td> ${memberList[findId].gender} </td>
@@ -43,7 +69,7 @@ function _inquiry(){    console.log('_inquiry()');
                         <td><button onclick="_delete(${findIq})" type=button""> 삭제</button></td>
                     </tr>
         `;
-    }       
+    }
 
     console.log(findId);
     console.log(findIq);
@@ -51,22 +77,24 @@ function _inquiry(){    console.log('_inquiry()');
     // 출력
     inquiryBox.innerHTML = html;
 }
-function _change(findIq){ console.log(findIq);
-    if(inquiryList[findIq].state == 'X'){
-        inquiryList[findIq].state = 'O'
-    }else{
-        inquiryList[findIq].state = 'X'
-    }                                           console.log(inquiryList);
+function _change(findIq) {
+    console.log(findIq);
+    if (inquiryList[findIq].state == 'X') {
+        inquiryList[findIq].state = 'O';
+    } else {
+        inquiryList[findIq].state = 'X';
+    } console.log(inquiryList);
     // 새로운 데이터를 배열에 저장했으면 localStorage 다시 저장한다.
-    localStorage.setItem('inquiryList' , JSON.stringify( inquiryList ));
+    localStorage.setItem('inquiryList', JSON.stringify(inquiryList));
 
     _inquiry();
 }
-function _delete(findIq){ console.log('_delete()');
-    inquiryList.splice(findIq , 1);
+function _delete(findIq) {
+    console.log('_delete()');
+    inquiryList.splice(findIq, 1);
 
     // 새로운 데이터를 배열에 저장했으면 localStorage 다시 저장한다.
-    localStorage.setItem('inquiryList' , JSON.stringify( inquiryList ));
+    localStorage.setItem('inquiryList', JSON.stringify(inquiryList));
 
     _inquiry();
 }
